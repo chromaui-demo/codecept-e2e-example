7757cd339188778e9cc974806046506c4e469ac6
ethriel3695 <ethriel3695@gmail.com>
2024-07-09 13:22:57 -0600

    update image

--- tests/mockSites.spec.ts
+++ tests/mockSites.spec.ts
@@ -3,5 +3,7 @@ import { test, expect, takeSnapshot } from '@chromatic-com/playwright';
 test.use({ diffThreshold: 0.3, viewport: { width: 400, height: 200 } });
 // Then use as normal 👇
 test('Vocera Loading Screen', async ({ page }, testInfo) => {
-  await page.goto('http://[::]:8000/public/baseline_image.png');
+  await page.goto(
+    'https://www.flickr.com/photos/201004774@N05/53846566029/in/dateposted-public/lightbox/'
+  );
 });
