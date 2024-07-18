6a1abbd24a44ceeebd491a60a060e74f833fa717
ethriel3695 <ethriel3695@gmail.com>
2024-07-18 07:53:40 -0600

    changed to failing image

--- tests/mockSites.spec.ts
+++ tests/mockSites.spec.ts
@@ -4,6 +4,6 @@ test.use({ diffThreshold: 0.3, viewport: { width: 240, height: 320 } });
 // Then use as normal 👇
 test('Vocera Loading Screen', async ({ page }, testInfo) => {
   await page.goto(
-    'https://www.flickr.com/photos/201080411@N02/53864021312/in/dateposted-public/lightbox/'
+    'https://www.flickr.com/photos/201080411@N02/53865281184/in/dateposted-public/lightbox/'
   );
 });
