2713651da5af5d60b87e34d67ba835f43988f9ef
ethriel3695 <ethriel3695@gmail.com>
2024-07-18 08:03:05 -0600

    update threshold again

--- tests/mockSites.spec.ts
+++ tests/mockSites.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect, takeSnapshot } from '@chromatic-com/playwright';
 
-test.use({ diffThreshold: 0.5, viewport: { width: 240, height: 320 } });
+test.use({ diffThreshold: 0.6, viewport: { width: 240, height: 320 } });
 // Then use as normal 👇
 test('Vocera Loading Screen', async ({ page }, testInfo) => {
   await page.goto(
